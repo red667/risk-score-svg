e478b0d8a2d5683cbd1b878e4b5956c38dd56bb8
red <red667@gmail.com>
2010-01-21 19:04:29 +0100

    initial commit

---
+++ risk-score-cairo.rb
@@ -0,0 +1,196 @@
+#! /usr/bin/ruby1.9 -Ku
+
+require 'cairo'
+include Math
+
+def cairo_image_surface(name,w,h,bg=nil)
+    surface = Cairo::SVGSurface.new(name,w,h)
+    image = Cairo::Context.new(surface)
+    if bg
+	image.set_source_rgba(*bg)
+	image.paint
+    end
+    yield(image)
+end
+
+def polyline image, points
+    first = true
+    points.each do |xy|
+	if first
+	    image.move_to(*xy)
+	    first = false
+	else
+	    image.line_to(*xy)
+	end	
+    end
+    image.close_path
+    first = true
+end
+
+def scale image, parts, line
+    polyline(image,line)
+    #normalvektor
+    norm = line.last[0].abs + line.last[1].abs
+    nv = line.last.reverse.map {|x| x / norm}
+    nv[0] *= -1
+    
+    (1..parts).each do |i|
+	a = line.last.map {|x| x / parts * i}
+
+	a[0] += nv[0] * 25
+	a[1] += nv[1] * 25
+
+	b = Array.new
+	b << a[0] - (nv[0] * 50)
+	b << a[1] - (nv[1] * 50)
+
+	polyline(image,[a, b])
+    end
+end
+
+w = 3500
+h = 4500
+r = 1300
+lw = 10
+
+red    = [1.0,0.0,0.0,1]
+black  = [0.0,0.0,0.0,1]
+white  = [1.0,1.0,1.0,1]
+yellow = [1.0,0.6,0.0,1]
+grey   = [0.5,0.5,0.5,1]
+green  = [0.0,1.0,0.0,1]
+
+vars = ["V","E","S","H","Z"]
+score = Array.new
+filename = ""
+
+if ARGV.length <= 4
+    puts "Usage: risk-score-cairo.rb V E S H Z"
+    puts "Where V,E,S,H,Z are values from 1..10"
+    Process.exit
+end
+
+ARGV.each_index do |i|
+    score << ARGV[i].to_i
+    filename += "%02d" % ARGV[i].to_i
+    if i <= 3
+	filename << "-"
+    end
+end
+
+pentagon_points = Array.new
+score_points = Array.new
+(1..5).each do |n|
+    pentagon_points << [r * sin((2*PI/5)*n) ,
+	(-1) * r * cos((2*PI/5)*n)]	# [xn,yn]
+    score_points << pentagon_points.last.map {|x| x / 10 * score[n-1]}
+end 
+
+cairo_image_surface(filename + ".svg",w,h,white) do |image|
+    image.set_line_join(Cairo::LINE_JOIN_ROUND)
+    image.set_line_cap(Cairo::LINE_CAP_ROUND)
+    image.translate((w/2)+lw,r+lw+700)
+    
+    g = Cairo::RadialPattern.new(0,0,0,0,0,r)
+    g.add_color_stop_rgba(0.2,0.0,1.0,0.0,1)
+    g.add_color_stop_rgba(0.5,1.0,1.0,0.0,1)
+    g.add_color_stop_rgba(1,1.0,0.0,0.0,1)
+
+    polyline(image,pentagon_points)
+    image.set_source(black)               
+    image.set_line_width(lw)
+    image.stroke_preserve
+    image.set_source(grey)
+    image.set_line_width(0)
+    image.fill
+
+    polyline(image,score_points)
+    image.set_source(black)
+    image.set_line_width(lw)
+    image.stroke_preserve
+    image.set_source(g)
+    image.set_line_width(0)
+    image.fill
+    
+    pentagon_points.each do |i|
+	scale(image,10,[[0,0],i])
+    end
+    image.set_source(black)
+    image.set_line_width(lw)
+    image.stroke
+
+    image.select_font_face("Arial", Cairo::FONT_SLANT_NORMAL, Cairo::FONT_WEIGHT_BOLD)
+    image.set_font_size(150.0)
+    pentagon_points.each_index do |i|
+	text = vars[i] + "=" + score[i].to_s
+	extents = image.text_extents(text)
+	x = (pentagon_points[i][0] * 1.15) - (extents.width/2 + extents.x_bearing)
+	y = (pentagon_points[i][1] * 1.15) - (extents.height/2 + extents.y_bearing)
+	image.move_to(x,y)
+	image.set_source(black)
+	image.set_line_width(1)
+	image.text_path(text)
+	image.fill_preserve
+	image.stroke
+    end
+    
+    image.set_font_size(250.0)
+    rs = score.inject(0) { |s,v| s += v } / 5.00
+    text = "Riskscore = " + rs.to_s
+    extents = image.text_extents(text)
+    x = 0 -(extents.width/2 + extents.x_bearing)
+    y = -h/2 + 550
+    image.move_to(x,y)
+    if rs >= 8
+	image.set_source(red)
+    elsif rs >= 5
+	image.set_source(yellow)
+    else
+	image.set_source(green)
+    end
+    image.set_line_width(1)
+    image.text_path(text)
+    image.fill_preserve
+    image.stroke
+
+    x += 420
+    y += 3300
+    image.move_to(x, y)
+    image.set_font_size(100)
+    image.set_source(black)
+    image.set_line_width(1)
+    text = "V .. Verbreitung"
+    image.text_path(text)
+    image.fill_preserve
+    image.stroke
+    y += 150
+    image.move_to(x, y)
+    text = "E .. Einfachheit"
+    image.text_path(text)
+    image.fill_preserve
+    image.stroke
+    y += 150
+    image.move_to(x, y)
+    text = "S .. Schadenspotenzial"
+    image.text_path(text)
+    image.fill_preserve
+    image.stroke
+    y += 150
+    image.move_to(x, y)
+    text = "H .. Häufigkeit"
+    image.text_path(text)
+    image.fill_preserve
+    image.stroke
+    y += 150
+    image.move_to(x, y)
+    text = "Z .. Zeitaufwand"
+
+    image.text_path(text)
+    image.fill_preserve
+    image.stroke
+    
+    #image.target.write_to_png(filename + ".png")
+
+end
+
+puts "Wrote: " + filename
